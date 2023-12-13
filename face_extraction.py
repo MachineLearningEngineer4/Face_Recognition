e6d1bc8c5c161bdef9025d34dfbd1b57b10f800b
MachineLearningEngineer4 <153170934+MachineLearningEngineer4@users.noreply.github.com>
2023-12-13 10:15:20 +0600

    correction

--- face_extraction.py
+++ face_extraction.py
@@ -1,6 +1,5 @@
 import cv2
 import glob
-import numpy as np
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
@@ -11,17 +10,12 @@ img_list = glob.glob(path)
 
 for file in img_list:
     img = cv2.imread(file, 1)
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    arr = np.asarray(gray)
-    faces = face_cascade.detectMultiScale(arr, 1.3, 5)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     try:
-        for (x, y, w, h) in faces:
-            roi_color = img[y:y+h, x:x+w]
-        resized = cv2.resize(roi_color, (128, 128))
-        print(resized)
-        cv2.imwrite("extracted_faces" + str(img_number) + ".jpg", resized)
+        resized = cv2.resize(gray, (128, 128))
+        cv2.imwrite("bpm" + str(img_number) + ".jpg", resized)
         print("done")
+        img_number += 1
     except:
         print("No faces detected")
 
-    img_number += 1
